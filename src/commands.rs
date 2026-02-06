93355de8c1c7e2d7c0277fe7bf5e3cb82abe312b
mentalblood0 <necheporenko.stepan@yandex.ru>
2026-02-06 16:02:42 +0300

    making commands iterator instead of one-shot parse function

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,8 @@
 use std::collections::{BTreeMap, BTreeSet};
+use std::io::{BufRead, BufReader, Read};
 
-use anyhow::{Context, Result, anyhow};
+use anyhow::{Context, Error, Result, anyhow};
+use fallible_iterator::FallibleIterator;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use trove::ObjectId;
@@ -81,65 +83,91 @@ pub enum Command {
 static COMMANDS_SPLIT_REGEX: std::sync::OnceLock<Regex> = std::sync::OnceLock::new();
 static COMMAND_FIRST_LINE_REGEX: std::sync::OnceLock<Regex> = std::sync::OnceLock::new();
 
-pub fn parse_commands_batch(
-    input: &str,
-    supported_relations_kinds: &BTreeSet<RelationKind>,
-) -> Result<Vec<Command>> {
-    let commands_split_regex = COMMANDS_SPLIT_REGEX.get_or_init(|| {
-        Regex::new(r#"(\r?\n|\r){2,}"#)
-            .with_context(|| "Can not compile regular expression for commands splitting")
-            .unwrap()
-    });
-    let command_first_line_regex = COMMAND_FIRST_LINE_REGEX.get_or_init(|| {
-        Regex::new(r#"^ *(\+|-|#|\^) +([^ ]+) *$"#)
-            .with_context(|| "Can not compile regular expression for commands splitting")
-            .unwrap()
-    });
-    let mut result = Vec::new();
-    let mut alias_to_index_in_result: BTreeMap<Alias, usize> = BTreeMap::new();
-    for (paragraph_index, paragraph) in commands_split_regex
-        .split(input)
-        .map(|paragraph| paragraph.trim())
-        .filter(|paragraph| !paragraph.is_empty())
-        .enumerate()
-    {
-        let lines = paragraph.split('\n').collect::<Vec<_>>();
-        if let Some(captures) = command_first_line_regex.captures(lines[0]) {
-            let operation_char = captures[1].chars().next().unwrap();
-            let alias_option = captures
-                .get(1)
-                .map(|alias_match| Alias(alias_match.as_str().to_string()));
-            if let Some(alias) = alias_option {
-                alias.validate().with_context(|| {
-                    format!(
-                        "Can not parse first line {:?} in {}-nth paragraph {:?}",
+struct CommandsIterator<'a> {
+    paragraphs_iterator: Box<dyn FallibleIterator<Item = (usize, &'a str), Error = Error> + 'a>,
+    supported_relations_kinds: &'a BTreeSet<RelationKind>,
+    aliases: BTreeSet<Alias>,
+}
+
+impl<'a> CommandsIterator<'a> {
+    pub fn new(input: &'a str, supported_relations_kinds: &'a BTreeSet<RelationKind>) -> Self {
+        let commands_split_regex = COMMANDS_SPLIT_REGEX.get_or_init(|| {
+            Regex::new(r#"(\r?\n|\r){2,}"#)
+                .with_context(|| "Can not compile regular expression for commands splitting")
+                .unwrap()
+        });
+        Self {
+            paragraphs_iterator: Box::new(fallible_iterator::convert(
+                commands_split_regex
+                    .split(input)
+                    .map(|paragraph| paragraph.trim())
+                    .filter(|paragraph| !paragraph.is_empty())
+                    .enumerate()
+                    .map(|index_and_paragraph| Ok(index_and_paragraph)),
+            )),
+            supported_relations_kinds,
+            aliases: BTreeSet::new(),
+        }
+    }
+}
+
+impl<'a> FallibleIterator for CommandsIterator<'a> {
+    type Item = Command;
+    type Error = Error;
+
+    fn next(&mut self) -> Result<Option<Self::Item>> {
+        if let Some((paragraph_index, paragraph)) = self.paragraphs_iterator.next()? {
+            let lines = paragraph.split('\n').collect::<Vec<_>>();
+            let command_first_line_regex = COMMAND_FIRST_LINE_REGEX.get_or_init(|| {
+                Regex::new(r#"^ *(\+|-|#|\^) +([^ ]+) *$"#)
+                    .with_context(|| "Can not compile regular expression for commands splitting")
+                    .unwrap()
+            });
+            if let Some(captures) = command_first_line_regex.captures(lines[0]) {
+                let operation_char = captures[1].chars().next().unwrap();
+                let alias_option = captures
+                    .get(1)
+                    .map(|alias_match| Alias(alias_match.as_str().to_string()));
+                if let Some(alias) = alias_option {
+                    alias.validate().with_context(|| {
+                        format!(
+                            "Can not parse first line {:?} in {}-nth paragraph {:?}",
+                            lines[0],
+                            paragraph_index + 1,
+                            paragraph
+                        )
+                    })?;
+                }
+                Ok(Some(match (operation_char, lines.len()) {
+                    ('+', 2) => Ok(Command::AddTextThesis(AddTextThesis {
+                        alias: alias_option,
+                        text: Text(lines[1].to_string()),
+                    })),
+                    ('+', 4) => Ok(Command::AddRelationThesis(AddRelationThesis {
+                        alias: alias_option,
+                        from: (),
+                        to: (),
+                        kind: (),
+                    })),
+                    _ => Err(anyhow!(
+                        "Unsupported operation character and lines count combination ({:?}, {}) in first line {:?} of {}-nth paragraph {:?}, supported combinations are ('+', 2) for adding text thesis, ('+', 4) for adding relation thesis, ('-', 2) for removing thesis, ('#', 3) for adding tag, ('^', 3) for removing tag",
+                        operation_char,
+                        lines.len(),
                         lines[0],
                         paragraph_index + 1,
                         paragraph
-                    )
-                })?;
-            }
-            let command = match (operation_char, lines.len()) {
-                ('+', 2) => Ok(Command::AddTextThesis(AddTextThesis {
-                    alias: alias_option,
-                    text: Text(lines[1].to_string()),
-                })),
-                ('+', 4) => Ok(Command::AddRelationThesis(AddRelationThesis {
-                    alias: alias_option,
-                    from: (),
-                    to: (),
-                    kind: (),
-                })),
-                _ => Err(anyhow!(
-                    "Unsupported operation character and lines count combination ({:?}, {}) in first line {:?} of {}-nth paragraph {:?}, supported combinations are ('+', 2) for adding text thesis, ('+', 4) for adding relation thesis, ('-', 2) for removing thesis, ('#', 3) for adding tag, ('^', 3) for removing tag",
-                    operation_char,
-                    lines.len(),
+                    )),
+                }?))
+            } else {
+                Err(anyhow!(
+                    "Can not parse first line {:?} in {}-nth paragraph {:?}",
                     lines[0],
                     paragraph_index + 1,
                     paragraph
-                )),
-            }?;
+                ))
+            }
+        } else {
+            Ok(None)
         }
     }
-    Ok(result)
 }
