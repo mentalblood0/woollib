71ebeda1bd3f2406cb6d8ac9a4e7a0b242944ce8
mentalblood0 <necheporenko.stepan@yandex.ru>
2026-02-11 16:35:11 +0300

    mentions now integrated in thesis itself

--- src/thesis.rs
+++ src/thesis.rs
@@ -1,11 +1,11 @@
-use anyhow::{Context, Result};
-use regex::Regex;
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use trove::ObjectId;
 
 use crate::alias::Alias;
 use crate::content::Content;
 use crate::mention::Mention;
+use crate::relation::Relation;
 use crate::tag::Tag;
 use crate::text::Text;
 
@@ -34,31 +34,17 @@ impl Thesis {
         Ok(self)
     }
 
-    pub fn mentions(&self) -> Result<Vec<Mention>> {
+    pub fn mentions(&self) -> Vec<ObjectId> {
         match self.content {
-            Content::Text(Text(ref text)) => {
-                static MENTION_IN_TEXT_REGEX: std::sync::OnceLock<Regex> =
-                    std::sync::OnceLock::new();
-                let mention_regex = MENTION_IN_TEXT_REGEX.get_or_init(|| {
-                    Regex::new(r"@([A-Za-z0-9-_]{22})[ ,$]")
-                        .with_context(
-                            || "Can not compile regular expression to search text for mentions",
-                        )
-                        .unwrap()
-                });
-                let self_id = self.id()?;
-                let mut result = vec![];
-                for capture in mention_regex.captures_iter(text) {
-                    result.push(Mention {
-                        mentioned: serde_json::from_str(&format!("\"{}\"", &capture[1]))?,
-                        inside: self_id.clone(),
-                    });
-                }
-                result.sort_by_key(|mention| mention.mentioned.clone());
-                result.dedup_by_key(|mention| mention.mentioned.clone());
-                Ok(result)
-            }
-            Content::Relation(_) => Ok(vec![]),
+            Content::Text(Text {
+                raw_text_parts: _,
+                ref references,
+            }) => references.clone(),
+            Content::Relation(Relation {
+                ref from,
+                ref to,
+                kind: _,
+            }) => vec![from.clone(), to.clone()],
         }
     }
 }
